383412156df05cf26311824c6d9bbf63376c97ef
jry <ijry@qq.com>
2026-06-01 12:04:18 +0800

    feat(admin): Forbidden 页改写为 NResult 403 + 切换账号

--- web/admin/src/views/Forbidden.vue
+++ web/admin/src/views/Forbidden.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
-// TODO: 待实现
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { NResult, NButton, NSpace } from 'naive-ui'
+import { useUserStore } from '@/stores/user'
+
+const router = useRouter()
+const { t } = useI18n()
+const userStore = useUserStore()
+
+async function onRelogin() {
+  await userStore.logout()
+  router.push('/login')
+}
 </script>
+
 <template>
-  <div class="p-6 text-fg-muted">页面建设中…</div>
+  <NResult status="403" :title="t('forbidden.title')" :description="t('forbidden.desc')">
+    <template #footer>
+      <NSpace>
+        <NButton @click="router.push('/')">{{ t('common.back_home') }}</NButton>
+        <NButton type="primary" @click="onRelogin">{{ t('auth.relogin') }}</NButton>
+      </NSpace>
+    </template>
+  </NResult>
 </template>
